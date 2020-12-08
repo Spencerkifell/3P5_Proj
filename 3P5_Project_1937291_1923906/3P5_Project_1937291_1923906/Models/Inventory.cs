90a611864f7fa2851692a6be80de2b1018a2c386
Spencer <spencerkfl@gmail.com>
2020-12-08 17:10:08 -0500

    Implementation of UML Diagram

---
+++ 3P5_Project_1937291_1923906/3P5_Project_1937291_1923906/Models/Inventory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3P5_Project_1937291_1923906.Models
+{
+    class Inventory
+    {
+        private List<Item> _items;
+
+        public Inventory() { }
+
+        public List<Item> Items
+        {
+            get { return _items; }
+            private set { _items = value; }
+        }
+
+        public string GeneralReport()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ShoppingList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool LoadItems(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveItems(string destination)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 3P5_Project_1937291_1923906/3P5_Project_1937291_1923906/Models/Item.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3P5_Project_1937291_1923906.Models
+{
+    class Item
+    {
+        private string _itemName;
+        private int _availableQuantity;
+        private int _minimumQuanity;
+        private string _location;
+        private List<string> _supplier;
+        private Category _itemCategory;
+        
+        public enum Category 
+        {
+            Computers,
+            Components,
+            Monitors,
+            Printers,
+            Security,
+            Cameras,
+            Headphones,
+            Games,
+            Phones,
+            Cables
+        }
+
+        public Item() { }
+        
+        public Item(int availableQuanity_, int minimumQuantity_, string location_, string supplier_, Category category_)
+        {
+            AvailableQuanity = availableQuanity_;
+            MinimumQuanity = minimumQuantity_;
+            Location = location_;
+            Supplier.Add(supplier_);
+            ItemCategory = category_;
+        }
+
+        public string ItemName
+        {
+            get { return _itemName; }
+            private set { _itemName = value; }
+        }
+
+        public int AvailableQuanity
+        {
+            get { return _availableQuantity; }
+            private set
+            {
+                if (value >= 0)
+                    _availableQuantity = value;
+                else
+                    throw new ArgumentException("Value cannot be negative.", "_availableQuanity");
+            }
+        }
+
+        public int MinimumQuanity
+        {
+            get { return _minimumQuanity; }
+            private set
+            {
+                if (value >= 1)
+                    _minimumQuanity = value;
+                else
+                    throw new ArgumentException("Value cannot be less than 1", "_minimumQuantity");
+            }
+        }
+
+        public string Location
+        {
+            get { return _location; }
+            private set { _location = value; }
+        }
+
+        public List<string> Supplier
+        {
+            get { return _supplier; }
+            private set { _supplier = value; }
+        }
+
+        public Category ItemCategory
+        {
+            get { return _itemCategory;  }
+            private set
+            {
+                //if((int)_itemCategory >= 0 && (int)_itemCategory <= )
+                _itemCategory = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
