0389f1994c14337837c3b6f18132675d30002e21
Venoshulk <arthurmousatov@gmail.com>
2020-12-08 18:26:51 -0500

    Added LoadItem Method

--- 3P5_Project_1937291_1923906/3P5_Project_1937291_1923906/Models/Inventory.cs
+++ 3P5_Project_1937291_1923906/3P5_Project_1937291_1923906/Models/Inventory.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace _3P5_Project_1937291_1923906.Models
 {
@@ -28,12 +29,25 @@ namespace _3P5_Project_1937291_1923906.Models
             throw new NotImplementedException();
         }
 
-        public bool LoadItems(string filePath)
+        public void LoadItems(string filePath)
         {
-            throw new NotImplementedException();
+            try
+            {
+                string[] newItems = File.ReadAllLines(filePath);
+
+                foreach(string data in newItems)
+                {
+                    Item itm = new Item();
+                    itm.ItemData = data;
+                    Items.Add(itm);
+                }
+            }catch(Exception e)
+            {
+                throw e;
+            }
         }
 
-        public bool SaveItems(string destination)
+        public void SaveItems(string destination)
         {
             throw new NotImplementedException();
         }
